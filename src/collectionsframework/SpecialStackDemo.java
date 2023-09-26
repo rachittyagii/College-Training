62b6eb59e8a1a7ec771c1cd314d57f9bc5bc9b65
Rachit Tyagi <rachittyagi@duck.com>
2023-09-26 15:53:31 +0530

    commit

--- src/collectionsframework/SpecialStackDemo.java
+++ src/collectionsframework/SpecialStackDemo.java
@@ -5,12 +5,14 @@ import java.util.Stack;
 public class SpecialStackDemo {
     public static void main(String[] args) {
         Stack<String> books = new Stack<>();
+        System.out.println("Default capacity: " + books.capacity());
         books.push("Grey");
         books.push(" Red");
         books.push("Blue");
         books.push("Black");
         books.push("White");
         books.push("Pink");
+        books.push("White");
         System.out.println("Stack of books: " + books);
         System.out.println("pop: " + books.pop());
         System.out.println("pop: " + books.pop());
